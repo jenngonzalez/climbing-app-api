d5cbd79916e022709df92e99717a7b53009821f2
Jenn Gonzalez <jgonz9873@gmail.com>
2019-10-22 19:36:47 -0700

    CORS setup

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config')
 
 const app = express()
 
@@ -13,12 +13,16 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+)
 
 
 
 app.get('/', (req, res) => {
-    res.send('Hello, boilerplate!')
+    res.send('Climbing App Server Running!')
 })
 
 app.get('/api/*', (req, res) => {
